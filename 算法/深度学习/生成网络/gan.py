a0548218fdd3b1672d85ace8e29be0d6a282e3ff
tianjian <jian.tian@xeniro.com>
2023-05-10 18:24:02 +0800

    add gan

--- 算法/深度学习/生成网络/gan.py
+++ 算法/深度学习/生成网络/gan.py
@@ -1,104 +1,122 @@
 import torch
 import torch.nn as nn
-import torchvision.datasets as dsets
+import torch.optim as optim
 import torchvision.transforms as transforms
-from torch.autograd import Variable
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import os
+
+"""
+这个简单的GAN网络使用MNIST数据集训练，可以在PyTorch上轻松实现。在训练过程中，首先初始化生成器和判别器网络，并定义优化器和损失函数。然后通过循环迭代训练数据集，每个迭代中先训练判别器，再训练生成器，最后输出训练信息并保存模型。
+
+"""
 
 # 定义生成器网络
 class Generator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, z_dim, img_dim):
         super(Generator, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
-        self.relu = nn.ReLU()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 1024),
+            nn.LeakyReLU(0.2),
+            nn.Linear(1024, img_dim),
+            nn.Tanh()
+        )
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
-        out = self.relu(out)
-        out = self.fc3(out)
-        return out
+        return self.gen(x)
+
 
 # 定义判别器网络
 class Discriminator(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, img_dim):
         super(Discriminator, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, output_size)
-        self.sigmoid = nn.Sigmoid()
+        self.disc = nn.Sequential(
+            nn.Linear(img_dim, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.sigmoid(out)
-        out = self.fc2(out)
-        out = self.sigmoid(out)
-        out = self.fc3(out)
-        out = self.sigmoid(out)
-        return out
+        return self.disc(x)
 
-# 定义超参数
-input_size = 784 # MNIST图像大小为28x28，因此输入大小为784
-hidden_size = 256
-output_size = 1 # 判别器的输出为一个二进制值（真/假）
-num_epochs = 200
-batch_size = 100
-learning_rate = 0.0002
 
-# 加载MNIST数据集
-train_dataset = dsets.MNIST(root='./data/',
-                            train=True,
-                            transform=transforms.ToTensor(),
-                            download=True)
+# 定义训练函数
+def train(gen, disc, dataloader, epochs, z_dim, lr, device):
+    # 初始化优化器和损失函数
+    opt_gen = optim.Adam(gen.parameters(), lr=lr)
+    opt_disc = optim.Adam(disc.parameters(), lr=lr)
+    criterion = nn.BCELoss()
 
-# 定义数据加载器
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=True)
+    for epoch in range(epochs):
+        for batch_idx, (real, _) in enumerate(dataloader):
+            # 初始化数据
+            real = real.view(-1, 784).to(device)
+            batch_size = real.shape[0]
+            # 定义真假标签
+            real_label = torch.ones(batch_size, 1).to(device)
+            fake_label = torch.zeros(batch_size, 1).to(device)
 
-# 定义生成器和判别器
-G = Generator(input_size, hidden_size, output_size)
-D = Discriminator(input_size, hidden_size, output_size)
+            # 训练判别器
+            noise = torch.randn(batch_size, z_dim).to(device)
+            fake = gen(noise)
+            disc_real = disc(real)
+            disc_fake = disc(fake.detach())
+            loss_disc_real = criterion(disc_real, real_label)
+            loss_disc_fake = criterion(disc_fake, fake_label)
+            loss_disc = (loss_disc_real + loss_disc_fake) / 2
+            disc.zero_grad()
+            loss_disc.backward()
+            opt_disc.step()
 
-# 定义损失函数和优化器
-criterion = nn.BCELoss() # 二元交叉熵损失函数
-G_optimizer = torch.optim.Adam(G.parameters(), lr=learning_rate)
-D_optimizer = torch.optim.Adam(D.parameters(), lr=learning_rate)
+            # 训练生成器
+            noise = torch.randn(batch_size, z_dim).to(device)
+            fake = gen(noise)
+            disc_fake = disc(fake)
+            loss_gen = criterion(disc_fake, real_label)
+            gen.zero_grad()
+            loss_gen.backward()
+            opt_gen.step()
 
-# 训练GAN
-for epoch in range(num_epochs):
-    for i, (images, _) in enumerate(train_loader):
-        # 生成真样本和假样本的标签
-        real_labels = torch.ones(batch_size, 1)
-        fake_labels = torch.zeros(batch_size, 1)
+            # 输出训练信息
+            if batch_idx % 100 == 0:
+                print(f"Epoch [{epoch}/{epochs}] Batch [{batch_idx}/{len(dataloader)}] Loss D: {loss_disc:.4f}, Loss G: {loss_gen:.4f}")
 
-        # 训练判别器
-        real_images = Variable(images.view(batch_size, -1))
-        real_outputs = D(real_images)
-        real_loss = criterion(real_outputs, real_labels)
+    # 保存模型
+    if not os.path.exists("models/"):
+        os.makedirs("models/")
+    torch.save(gen.state_dict(), "models/generator.pth")
+    torch.save(disc.state_dict(), "models/discriminator.pth")
 
-        z = Variable
-        # 训练生成器
-        z = Variable(torch.randn(batch_size, input_size)) # 生成随机噪声
-        fake_images = G(z)
-        fake_outputs = D(fake_images)
-        fake_loss = criterion(fake_outputs, fake_labels)
 
-        G_loss = fake_loss
+if __name__ == '__main__':
+    # 超参数定义
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    z_dim = 64
+    lr = 0.0002
+    batch_size = 128
+    epochs = 10
+    img_dim = 784
 
-        D.zero_grad()
-        real_loss.backward()
-        D_optimizer.step()
+    # 下载数据集
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+    dataset = datasets.MNIST(root="./data", train=True, transform=transform, download=True)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-        G.zero_grad()
-        G_loss.backward()
-        G_optimizer.step()
+    # 初始化网络
+    gen = Generator(z_dim, img_dim).to(device)
+    disc = Discriminator(img_dim).to(device)
 
-        if (i + 1) % 100 == 0:
-            print('Epoch [%d/%d], Step [%d/%d], d_loss: %.4f, g_loss: %.4f'
-                  % (epoch + 1, num_epochs, i + 1, len(train_dataset) // batch_size, real_loss.data[0], G_loss.data[0]))
+    # 训练网络
+    train(gen, disc, dataloader, epochs, z_dim, lr, device)
 
-# 保存生成器模型
-torch.save(G.state_dict(), 'generator.pkl')
